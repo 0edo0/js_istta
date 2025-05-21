e37d8534a0de881c40a83bec08c9a4a142fd6e3a
0edo0 <edwinmolleapazakcanchay@gmail.com>
2025-05-21 09:25:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 function fondo() {
     document.body.style.backgroundColor = "black";
+    const nav = document.querySelector(".navbar");
+
+    if (nav) {
+        nav.style.color = "cyan";
+        const links = nav.querySelectorAll("a");
+        links.forEach(link => {
+            link.style.color = "cyan";
+        });
+    }
+
+    const titulo=document.querySelector("h3");
+    if(titulo){
+        titulo.style.color="white"
+    }
 
 }
 function fondo2() {
     document.body.style.backgroundColor = "white";
+       const nav = document.querySelector(".navbar");
+
+    if (nav) {
+        nav.style.color = "";
+        const links = nav.querySelectorAll("a");
+        links.forEach(link => {
+            link.style.color = "";
+        });
+    }
+
+    const titulo=document.querySelector("h3");
+    if(titulo){
+        titulo.style.color=""
+    }
+
 }
 
 function mensaje(){
