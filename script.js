16eac285c8d6fe70ecf592941cc50d18800442ab
0edo0 <edwinmolleapazakcanchay@gmail.com>
2025-05-20 22:45:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function fondo() {
+    document.body.style.backgroundColor = "black";
+
+}
+function fondo2() {
+    document.body.style.backgroundColor = "white";
+}
+
+function mensaje(){
+     document.getElementById("parrafo").innerText =
+"¡Bienvenido!"; 
+}
